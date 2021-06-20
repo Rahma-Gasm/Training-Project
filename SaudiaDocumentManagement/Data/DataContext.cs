5c8008a5bfae1eab0fcfa902b81e59c477e6cc95
Moayad <moayad0ab@protonmail.com>
2021-06-20 22:17:20 +0300

    Db context and migrations

---
+++ SaudiaDocumentManagement/Data/DataContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using SaudiaDocumentManagment;
+
+namespace SaudiaDocumentManagement.Models
+{
+public class DataContext : DbContext
+{
+
+    public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+    {
+
+    }
+
+    public DbSet<admin> admin { get; set; }
+    public DbSet<category> category { get; set; }
+    public DbSet<files> files { get; set; }
+    public DbSet<activity_list> activity_List { get; set; }
+           
+}
+}
---
+++ SaudiaDocumentManagement/Migrations/20210620190522_InitialCreate.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SaudiaDocumentManagement.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "activity_List",
+                columns: table => new
+                {
+                    activity_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    admin_name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    update_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    category_name = table.Column<int>(type: "int", nullable: false),
+                    sub_category_name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    process_name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    file_name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_activity_List", x => x.activity_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "admin",
+                columns: table => new
+                {
+                    admin_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    date_of_birth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    mobile_number = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_admin", x => x.admin_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "category",
+                columns: table => new
+                {
+                    category_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    category_name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    parent_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_category", x => x.category_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "files",
+                columns: table => new
+                {
+                    file_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    file_number = table.Column<int>(type: "int", nullable: false),
+                    file_category = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    pdf_file = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_files", x => x.file_id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "activity_List");
+
+            migrationBuilder.DropTable(
+                name: "admin");
+
+            migrationBuilder.DropTable(
+                name: "category");
+
+            migrationBuilder.DropTable(
+                name: "files");
+        }
+    }
+}
