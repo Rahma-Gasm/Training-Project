a499c9386036407279a4d1657b831f7a1542587a
RAGHAD MQBOOL AL HARBI <1807934@stu.kau.edu.sa>
2021-06-30 14:05:30 +0300

    add admin controller and develop it+ add CreateRoleViewModel + add new View folder "admin" and createrole in it

---
+++ SaudiaDocumentManagement/ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaudiaDocumentManagement.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        public String RoleName { get; set; }
+
+
+
+
+    }
+}
